5283e1f2cdc8c22cca3c9699e825ffb13b7836c2
KeiraKistensamy <Keirakist0987@gmail.com>
2024-08-06 15:30:23 +0200

    APIContent

--- config/index.js
+++ config/index.js
@@ -6,11 +6,11 @@ let connection = createPool ({
     user: process.env.userDb,
     password: process.env.pwdDb,
     database: process.env.dbName,
-    multipleStatement: true,
+    multipleStatements: true,
     connectionLimit: 30
 })
-connection.on('connection', (err) => {
-    if(err) throw new Error('Couldn\t connect to the database, please try again later')
+connection.on('connection', (pool) => {
+    if(!pool) throw new Error('Couldn\t connect to the database, please try again later')
 })
 export {
     connection
