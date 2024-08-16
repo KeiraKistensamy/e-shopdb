2728a372ecbfd25755c0624c52dff9d797460322
KeiraKistensamy <Keirakist0987@gmail.com>
2024-08-16 15:51:07 +0200

    NavBar and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  
+    <div class="container-fluid">
+        <div class="row">
+            <p class="lead">
+                KK @copy; <span id="CYear"></span>
+            </p>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  
+<nav class="navbar navbar-expand-lg">
+<div class="container-fluid">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://keirakistensamy.github.io/allimages/images/KeiraLogo.jpg">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+        <li class="nav-item"><a class="nav-link" aria-disabled="true">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
